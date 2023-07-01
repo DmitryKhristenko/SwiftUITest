eeaae01580af062a33bd4c91c0ab5a84a22e4ba4
Dmitry Khristenko <dmitrykhrist@gmail.com>
2023-07-01 14:47:28 +0300

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Дмитрий Х on 01.07.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
