c53e694ac1450fb7749215dcc85dfd9427f5775e
Dmitry Khristenko <dmitrykhrist@gmail.com>
2023-08-13 16:20:27 +0300

    Change structure, add new files and features.

---
+++ SwiftUITest/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by Дмитрий Х on 01.07.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITest/Views/LandmarkList.swift
@@ -0,0 +1,40 @@
+//
+//  LandmarkList.swift
+//  SwiftUITest
+//
+//  Created by Дмитрий Х on 10.08.23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle("Favourites only", isOn: $showFavoritesOnly)
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
