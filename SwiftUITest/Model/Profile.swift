e59ec1c38fe2723234444f4b51d9a4ca1ad93ff5
Dmitry Khristenko <dmitrykhrist@gmail.com>
2023-08-15 15:02:35 +0300

    Add Profiles group, profile file, HikeBadge, profile edit

---
+++ SwiftUITest/Model/Profile.swift
@@ -0,0 +1,29 @@
+//
+//  Profile.swift
+//  SwiftUITest
+//
+//  Created by Дмитрий Х on 15.08.23.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+
+    static let `default` = Profile(username: "g_kumar")
+
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
---
+++ SwiftUITest/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,63 @@
+//
+//  ProfileSummary.swift
+//  SwiftUITest
+//
+//  Created by Дмитрий Х on 15.08.23.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    @EnvironmentObject var modelData: ModelData
+
+    var profile: Profile
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(profile.username)
+                    .bold()
+                    .font(.title)
+                
+                Text("Notifications: \(profile.prefersNotifications ? "On": "Off" )")
+                Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 255))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+                    
+                    HikeView(hike: modelData.hikes[0])
+                }
+            }
+        }
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary(profile: Profile.default)
+            .environmentObject(ModelData())
+    }
+}
